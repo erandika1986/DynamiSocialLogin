5969fb886f85fe14c67add167e1d552abfb1dc69
erandika86 <30391205+erandika86@users.noreply.github.com>
2018-10-26 13:16:23 +0530

    Third time checking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthServiceConfig } from './dynami-social-login/services/auth.service.config';
-import { GoogleLoginProvider } from './dynami-social-login/providers';
+import { GoogleLoginProvider, FacebookLoginProvider, LinkedinLoginProvider } from './dynami-social-login/providers';
 import { DynamiSocialLoginModule } from './dynami-social-login/dynami-social-login.module';
-
 
 export function getAuthServiceConfigs() {
   let config = new AuthServiceConfig(
       [
-/*         {
+         {
           id: FacebookLoginProvider.PROVIDER_ID,
-          provider: new FacebookLoginProvider("1799423350170966")
-        }, */
+          provider: new FacebookLoginProvider("XXXXXX")
+        },
         {
           id: GoogleLoginProvider.PROVIDER_ID,
-          provider: new GoogleLoginProvider("223238774590-69ala37v3q48rcdse6r5nqrd7hmtcukt.apps.googleusercontent.com")
+          provider: new GoogleLoginProvider("XXXxXXXX.apps.googleusercontent.com")
         }
-/*         ,
+         ,
           {
             id: LinkedinLoginProvider.PROVIDER_ID,
-            provider: new LinkedinLoginProvider("1098828800522-m2ig6bieilc3tpqvmlcpdvrpvn86q4ks.apps.googleusercontent.com")
-          }, */
+            provider: new LinkedinLoginProvider("XXXXXXXxXX")
+          },
       ]
   );
   return config;
